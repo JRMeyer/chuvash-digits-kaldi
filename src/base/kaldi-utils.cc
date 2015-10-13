kaldi-asr/kaldi

commit cb6635dfd49a555b8bd8008c35ecd64b73cc8310

make source and header files in /base comply with the Google style

src/base/kaldi-utils.cc
@@ -16,17 +16,17 @@
 // See the Apache 2 License for the specific language governing permissions and
 // limitations under the License.
 
-#include <string>
-#include "base/kaldi-common.h"
-
-
 #ifdef _WIN32_WINNT_WIN8
 #include <Synchapi.h>
-#elif defined (_WIN32) || defined(_MSC_VER) || defined(MINGW)
+#elif defined(_WIN32) || defined(_MSC_VER) || defined(MINGW)
 #include <Windows.h>
 #else
 #include <unistd.h>
 #endif
+
+#include <string>
+#include "base/kaldi-common.h"
+
 
 namespace kaldi {
 
@@ -35,7 +35,7 @@ std::string CharToString(const char &c) {
   if (std::isprint(c))
     sprintf(buf, "\'%c\'", c);
   else
-    sprintf(buf, "[character %d]", (int) c);
+    sprintf(buf, "[character %d]", static_cast<int>(c));
   return (std::string) buf;
 }
 

src/base/timer.h
@@ -19,15 +19,16 @@
 #ifndef KALDI_BASE_TIMER_H_
 #define KALDI_BASE_TIMER_H_
 
+# include <sys/time.h>
+# include <unistd.h>
+
 #include "base/kaldi-utils.h"
 // Note: Sleep(float secs) is included in base/kaldi-utils.h.
 
 
 #if defined(_MSC_VER) || defined(MINGW)
 
-namespace kaldi
-{
-
+namespace kaldi {
 class Timer {
  public:
   Timer() { Reset(); }
@@ -38,9 +39,11 @@ class Timer {
     LARGE_INTEGER time_end;
     LARGE_INTEGER freq;
     QueryPerformanceCounter(&time_end);
-    if (QueryPerformanceFrequency(&freq) == 0) return 0.0;  // Hardware does not support this.
-    return ((double)time_end.QuadPart - (double)time_start_.QuadPart) /
-        ((double)freq.QuadPart);
+    // Hardware does not support this.
+    if (QueryPerformanceFrequency(&freq) == 0) return 0.0;
+    return (static_cast<double>(time_end.QuadPart) -
+            static_cast<double>(time_start_.QuadPart)) /
+           (static_cast<double>(freq.QuadPart));
   }
  private:
   LARGE_INTEGER time_start_;
@@ -49,12 +52,8 @@ class Timer {
 
 #else
 
-# include <sys/time.h>
-# include <unistd.h>
-namespace kaldi
-{
-class Timer
-{
+namespace kaldi {
+class Timer {
  public:
   Timer() { Reset(); }
 
@@ -65,9 +64,10 @@ class Timer
     struct timeval time_end;
     gettimeofday(&time_end, &time_zone_);
     double t1, t2;
-    t1 =  (double)time_start_.tv_sec +
-        (double)time_start_.tv_usec/(1000*1000);
-    t2 =  (double)time_end.tv_sec + (double)time_end.tv_usec/(1000*1000);
+    t1 =  static_cast<double>(time_start_.tv_sec) +
+          static_cast<double>(time_start_.tv_usec)/(1000*1000);
+    t2 =  static_cast<double>(time_end.tv_sec) +
+          static_cast<double>(time_end.tv_usec)/(1000*1000);
     return t2-t1;
   }
 
@@ -80,4 +80,4 @@ class Timer
 #endif
 
 
-#endif
+#endif  // KALDI_BASE_TIMER_H_

src/base/io-funcs-inl.h
@@ -1,7 +1,8 @@
 // base/io-funcs-inl.h
 
 // Copyright 2009-2011  Microsoft Corporation;  Saarland University;
-//                      Jan Silovsky;   Yanmin Qian;  Johns Hopkins University (Author: Daniel Povey)
+//                      Jan Silovsky;   Yanmin Qian;
+//                      Johns Hopkins University (Author: Daniel Povey)
 
 // See ../../COPYING for clarification regarding multiple authors
 //
@@ -62,7 +63,6 @@ template<class T> inline void ReadBasicType(std::istream &is,
     char len_c = static_cast<char>(len_c_in), len_c_expected
       = (std::numeric_limits<T>::is_signed ? 1 :  -1)
       * static_cast<char>(sizeof(*t));
-    
     if (len_c !=  len_c_expected) {
       KALDI_ERR << "ReadBasicType: did not get expected integer type, "
                 << static_cast<int>(len_c)

src/base/io-funcs-test.cc
@@ -92,7 +92,8 @@ void UnitTestIo(bool binary) {
       KALDI_ASSERT(vec3_in == vec3);
       std::string  token1_in, token2_in;
       KALDI_ASSERT(Peek(infile, binary_in) == static_cast<int>(*token1));
-      KALDI_ASSERT(PeekToken(infile, binary_in) == (int)*token1); // Note:
+      KALDI_ASSERT(PeekToken(infile, binary_in) == static_cast<int>(*token1));
+      // Note:
       // the stuff with skipping over '<' is tested in ../util/kaldi-io-test.cc,
       // since we need to make sure it works with pipes.
       ReadToken(infile, binary_in, &token1_in);
@@ -127,12 +128,11 @@ void UnitTestIo(bool binary) {
 }  // end namespace kaldi.
 
 int main() {
-  using namespace kaldi;
   for (size_t i = 0; i < 10; i++) {
-    UnitTestIo(false);
-    UnitTestIo(true);
+    kaldi::UnitTestIo(false);
+    kaldi::UnitTestIo(true);
   }
-  KALDI_ASSERT(1);  // just wanted to check that KALDI_ASSERT does not fail for 1.
+  KALDI_ASSERT(1);  // just to check that KALDI_ASSERT does not fail for 1.
   return 0;
 }
 

src/base/kaldi-common.h
@@ -28,8 +28,8 @@
 #include <stdexcept>
 #include <cassert>
 #include <vector>
-#include <iostream>  
-#include <fstream>  
+#include <iostream>
+#include <fstream>
 
 #include "base/kaldi-utils.h"
 #include "base/kaldi-error.h"

src/base/kaldi-error-test.cc
@@ -46,7 +46,7 @@ int main() {
   try {
     kaldi::UnitTestError();
     KALDI_ASSERT(0);  // should not happen.
-  } catch (std::runtime_error &r) {
+  } catch(std::runtime_error &r) {
     std::cout << "UnitTestError: the error we generated was: " << r.what();
   }
 }

src/base/kaldi-error.cc
@@ -40,7 +40,8 @@ const char *g_program_name = NULL;
 // colon, e.g. "gmm-align:".  Otherwise it returns the empty string "".
 const char *GetProgramName() {
   if (g_program_name == NULL) return "";
-  else return g_program_name;
+  else
+    return g_program_name;
 }
 
 // Given a filename like "/a/b/c/d/e/f.cc",  GetShortFileName
@@ -48,8 +49,9 @@ const char *GetProgramName() {
 // the filename separator.
 const char *GetShortFileName(const char *filename) {
   const char *last_slash = strrchr(filename, '/');
-  if (!last_slash) { return filename; }
-  else {
+  if (!last_slash) {
+    return filename;
+  } else {
     while (last_slash > filename && last_slash[-1] != '/')
       last_slash--;
     return last_slash;
@@ -200,7 +202,7 @@ KaldiErrorMessage::~KaldiErrorMessage() KALDI_NOEXCEPT(false) {
     throw std::runtime_error(str);
 #endif
   } else {
-    abort(); // This may be temporary...
+    abort();  // This may be temporary...
   }
 }
 

src/base/kaldi-error.h
@@ -120,7 +120,8 @@ class KaldiErrorMessage {
 // Note on KALDI_ASSERT and KALDI_PARANOID_ASSERT
 // The original (simple) version of the code was this
 //
-// #define KALDI_ASSERT(cond) if (!(cond)) kaldi::KaldiAssertFailure_(__func__, __FILE__, __LINE__, #cond);
+// #define KALDI_ASSERT(cond) if (!(cond))
+//              kaldi::KaldiAssertFailure_(__func__, __FILE__, __LINE__, #cond);
 //
 // That worked well, but we were concerned that it
 // could potentially cause a performance issue due to failed branch
@@ -139,24 +140,27 @@ class KaldiErrorMessage {
 // and compilers will be able to optimize the loop away (as the condition
 // is always false).
 #ifndef NDEBUG
-#define KALDI_ASSERT(cond) \
-  do { if ((cond)) ; else kaldi::KaldiAssertFailure_(__func__, __FILE__, __LINE__, #cond);} while(0)
+#define KALDI_ASSERT(cond) do { if ((cond)) ;  else \
+  kaldi::KaldiAssertFailure_(__func__, __FILE__, __LINE__, #cond);}while(0)
 #else
 #define KALDI_ASSERT(cond)
 #endif
 // also see KALDI_COMPILE_TIME_ASSERT, defined in base/kaldi-utils.h,
 // and KALDI_ASSERT_IS_INTEGER_TYPE and KALDI_ASSERT_IS_FLOATING_TYPE,
 // also defined there.
-#ifdef KALDI_PARANOID // some more expensive asserts only checked if this defined
-#define KALDI_PARANOID_ASSERT(cond) \
-  do { if ((cond)) ; else kaldi::KaldiAssertFailure_(__func__, __FILE__, __LINE__, #cond);} while(0)
+// some more expensive asserts only checked if this defined
+#ifdef KALDI_PARANOID
+#define KALDI_PARANOID_ASSERT(cond) do { if ((cond)) ;  else \
+  kaldi::KaldiAssertFailure_(__func__, __FILE__, __LINE__, #cond);}while(0)
 #else
 #define KALDI_PARANOID_ASSERT(cond)
 #endif
 
 
-#define KALDI_ERR kaldi::KaldiErrorMessage(__func__, __FILE__, __LINE__).stream()
-#define KALDI_WARN kaldi::KaldiWarnMessage(__func__, __FILE__, __LINE__).stream()
+#define KALDI_ERR \
+  kaldi::KaldiErrorMessage(__func__, __FILE__, __LINE__).stream()
+#define KALDI_WARN \
+  kaldi::KaldiWarnMessage(__func__, __FILE__, __LINE__).stream()
 #define KALDI_LOG kaldi::KaldiLogMessage(__func__, __FILE__, __LINE__).stream()
 
 #define KALDI_VLOG(v) if (v <= kaldi::g_kaldi_verbose_level)     \

JRMeyer/chuvash-digits-kaldi

commit 41d972167aa70ba5a41e184567062decc213f439

Fixing the problems introduced by the commit cb6635dfd49a555b8bd8008c35ecd64b73cc8310

src/base/kaldi-utils.cc
@@ -20,6 +20,7 @@
 #include <Synchapi.h>
 #elif defined(_WIN32) || defined(_MSC_VER) || defined(MINGW)
 #include <Windows.h>
+#define snprintf _snprintf
 #else
 #include <unistd.h>
 #endif

src/base/timer.h
@@ -19,9 +19,6 @@
 #ifndef KALDI_BASE_TIMER_H_
 #define KALDI_BASE_TIMER_H_
 
-# include <sys/time.h>
-# include <unistd.h>
-
 #include "base/kaldi-utils.h"
 // Note: Sleep(float secs) is included in base/kaldi-utils.h.
 
@@ -39,8 +36,11 @@ class Timer {
     LARGE_INTEGER time_end;
     LARGE_INTEGER freq;
     QueryPerformanceCounter(&time_end);
-    // Hardware does not support this.
-    if (QueryPerformanceFrequency(&freq) == 0) return 0.0;
+
+    if (QueryPerformanceFrequency(&freq) == 0) {
+      //  Hardware does not support this.
+      return 0.0;
+    }
     return (static_cast<double>(time_end.QuadPart) -
             static_cast<double>(time_start_.QuadPart)) /
            (static_cast<double>(freq.QuadPart));
@@ -51,6 +51,8 @@ class Timer {
 }
 
 #else
+#include <sys/time.h>
+#include <unistd.h>
 
 namespace kaldi {
 class Timer {