770a2a28e40886da81de4305fa24b9c0c9fc3549
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-10-24 12:20:49 +0000

    experiments and new WER calculation script

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -2,7 +2,7 @@
 
 
 
-for run in {1..2}
+for run in {1..100}
 do
     time ./run_nnet3_multilingual.sh "org no_voice" "tri mono" "0.5,0.5" "${run}"
     time ./run_nnet3_multilingual.sh "org no_voice" "tri tri" "0.5,0.5" "${run}"
