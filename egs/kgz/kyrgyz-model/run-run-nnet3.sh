e1bf2460fc92687424ef71786e02d3a96abe0dae
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-10-25 07:40:59 +0000

    ran more experiments

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -2,8 +2,13 @@
 
 
 
-for run in {1..100}
+for run in {1..25}
 do
-    time ./run_nnet3_multilingual.sh "org no_voice" "tri mono" "0.5,0.5" "${run}"
-    time ./run_nnet3_multilingual.sh "org no_voice" "tri tri" "0.5,0.5" "${run}"
+    ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" "${run}"
+  
+    ./run_nnet3_multilingual.sh "org org2" "tri mono" "0.5,0.5" "${run}"
+  
+    ./run_nnet3_multilingual.sh "org no_voice" "tri mono" "0.5,0.5" "${run}"
+    
+    ./run_nnet3_multilingual.sh "org no_voice" "tri tri" "0.5,0.5" "${run}"
 done
