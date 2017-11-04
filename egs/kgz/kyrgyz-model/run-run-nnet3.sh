39a103cd97abff3957af6a448f971a1f2a14c08e
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-11-04 12:29:45 +0000

    more experiments with wikipedia language mode about 67megs and 3 dnn epochs

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -4,16 +4,20 @@
 
 for run in {1..20}
 do
-    ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" "${run}_tokmok"
+    ./run_nnet3_multilingual.sh "orgWiki org2" "tri tri" "0.5,0.5" "${run}"
   
-    ./run_nnet3_multilingual.sh "org org2" "tri mono" "0.5,0.5" "${run}_tokmok"
+    ./run_nnet3_multilingual.sh "orgWiki org2" "tri mono" "0.5,0.5" "${run}"
   
-    ./run_nnet3_multilingual.sh "org no_voice" "tri tri" "0.5,0.5" "${run}_tokmok"
+    ./run_nnet3_multilingual.sh "orgWiki manner" "tri tri" "0.5,0.5" "${run}"
   
-    ./run_nnet3_multilingual.sh "org no_voice" "tri mono" "0.5,0.5" "${run}_tokmok"
+    ./run_nnet3_multilingual.sh "orgWiki manner" "tri mono" "0.5,0.5" "${run}"
+
+    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri tri" "0.5,0.5" "${run}"
   
-    ./run_nnet3_multilingual.sh "org babel" "tri tri" "0.5,0.5" "${run}_tokmok"
+    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri mono" "0.5,0.5" "${run}"
+
+    ./run_nnet3_multilingual.sh "orgWiki babel" "tri tri" "0.5,0.5" "${run}"
   
-    ./run_nnet3_multilingual.sh "org babel" "tri mono" "0.5,0.5" "${run}_tokmok"
+    ./run_nnet3_multilingual.sh "orgWiki babel" "tri mono" "0.5,0.5" "${run}"
   
 done
