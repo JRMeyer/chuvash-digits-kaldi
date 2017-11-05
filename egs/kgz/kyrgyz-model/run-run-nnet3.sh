0732817c5875cb58cdb0ce4409beec8b070b4b9a
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-11-05 21:42:00 +0000

    more experiments

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -4,20 +4,16 @@
 
 for run in {1..20}
 do
-    ./run_nnet3_multilingual.sh "orgWiki org2" "tri tri" "0.5,0.5" "${run}"
+    ./run_nnet3_multilingual.sh "orgWiki org2" "tri tri" "0.5,0.5" "${run}_2gram"
   
-    ./run_nnet3_multilingual.sh "orgWiki org2" "tri mono" "0.5,0.5" "${run}"
+    ./run_nnet3_multilingual.sh "orgWiki org2" "tri mono" "0.5,0.5" "${run}_2gram"
   
-    ./run_nnet3_multilingual.sh "orgWiki manner" "tri tri" "0.5,0.5" "${run}"
+    ./run_nnet3_multilingual.sh "orgWiki manner" "tri tri" "0.5,0.5" "${run}_2gram"
   
-    ./run_nnet3_multilingual.sh "orgWiki manner" "tri mono" "0.5,0.5" "${run}"
+    ./run_nnet3_multilingual.sh "orgWiki manner" "tri mono" "0.5,0.5" "${run}_2gram"
+  
+    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri tri" "0.5,0.5" "${run}_2gram"
+  
+    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri mono" "0.5,0.5" "${run}_2gram"
 
-    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri tri" "0.5,0.5" "${run}"
-  
-    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri mono" "0.5,0.5" "${run}"
-
-    ./run_nnet3_multilingual.sh "orgWiki babel" "tri tri" "0.5,0.5" "${run}"
-  
-    ./run_nnet3_multilingual.sh "orgWiki babel" "tri mono" "0.5,0.5" "${run}"
-  
 done
