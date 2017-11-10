4409e9a9fedbff4874c5843635158e907f60d953
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-11-10 12:53:43 +0000

    new experiments and plotting

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -4,16 +4,16 @@
 
 for run in {1..20}
 do
-    ./run_nnet3_multilingual.sh "orgWiki org2" "tri tri" "0.5,0.5" "${run}_2gram"
+    ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" "${run}_2gram5epoch5layer100dim"
   
-    ./run_nnet3_multilingual.sh "orgWiki org2" "tri mono" "0.5,0.5" "${run}_2gram"
-  
-    ./run_nnet3_multilingual.sh "orgWiki manner" "tri tri" "0.5,0.5" "${run}_2gram"
-  
-    ./run_nnet3_multilingual.sh "orgWiki manner" "tri mono" "0.5,0.5" "${run}_2gram"
-  
-    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri tri" "0.5,0.5" "${run}_2gram"
-  
-    ./run_nnet3_multilingual.sh "orgWiki no_voice" "tri mono" "0.5,0.5" "${run}_2gram"
+    ./run_nnet3_multilingual.sh "org org2" "tri mono" "0.5,0.5" "${run}_2gram5epoch5layer100dim"
 
+    ./run_nnet3_multilingual.sh "org libri" "tri tri" "0.5,0.5" "${run}_2gram5epoch5layer100dim"
+  
+    ./run_nnet3_multilingual.sh "org libri" "tri mono" "0.5,0.5" "${run}_2gram5epoch5layer100dim"
+
+    ./run_nnet3_multilingual.sh "org no_voice" "tri tri" "0.5,0.5" "${run}_2gram5epoch5layer100dim"
+  
+    ./run_nnet3_multilingual.sh "org no_voice" "tri mono" "0.5,0.5" "${run}_2gram5epoch5layer100dim"
+  
 done
