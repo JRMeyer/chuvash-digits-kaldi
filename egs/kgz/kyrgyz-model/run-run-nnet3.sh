b62bc6b44e32468e116fcafac37c8c604a4848ca
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-11-12 07:31:48 +0000

    ran more experiments with mono no -voice

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -4,9 +4,9 @@
 
 for run in {1..20}
 do
-    for dim in 100 150 200 250 300 350 400 450 500 550 600
+    for dim in 100 150 200 250 300 350 400 450 500
     do
-        ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" $dim "2gram_5epoch_5layer_${dim}dim_${run}"
+        ./run_nnet3_multilingual.sh "org no_voice" "tri mono" "0.5,0.5" $dim "2gram_5epoch_5layer_${dim}dim_${run}"
         
     done
 done
