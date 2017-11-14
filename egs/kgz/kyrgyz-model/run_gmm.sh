0ecbbd1deb0f47914ec22c26c9e596979f43209b
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-11-14 18:23:12 +0000

    lots of changes

--- egs/kgz/kyrgyz-model/run_gmm.sh
+++ egs/kgz/kyrgyz-model/run_gmm.sh
@@ -51,12 +51,10 @@ fi
 ##
 #
 prep_data=1
-extract_feats=0
-train_monophones=0
-train_triphones=0
-adapt_models=0
-compile_graph=1
-decode_test=1
+extract_feats=1
+train_gmm=1
+compile_graph=0
+decode_test=0
 save_model=0
 #
 ##
@@ -66,10 +64,9 @@ save_model=0
 ### HYPER-PARAMETERS
 ##
 #
-train_percentage=.9
-tot_gauss_mono=500
-num_leaves_tri=500
-tot_gauss_tri=1000
+tot_gauss_mono=1000
+num_leaves_tri=1000
+tot_gauss_tri=2000
 decode_beam=13
 decode_lattice_beam=7
 decode_max_active_states=700
@@ -103,11 +100,11 @@ data_dir=data_${corpus_name}
 exp_dir=exp_${corpus_name}
 # mfcc_dir=mfcc_${corpus_name}
 plp_dir=plp_${corpus_name}
-train_dir=train
-test_dir=test
 #
 ##
 ###
+
+
 
 
 
@@ -117,71 +114,8 @@ if [ "$prep_data" -eq "1" ]; then
     printf "#### BEGIN DATA PREP ####\n";
     printf "####=================####\n\n";
 
-    echo "$0: looking for audio data in $audio_dir"
+    ./prep_data.sh $input_dir $audio_dir $data_dir
     
-    # Make sure we have the audio data (WAV file utterances)
-    if [ ! -d $audio_dir ]; then
-        printf '\n####\n#### ERROR: '"${audio_dir}"' not found \n####\n\n';
-        exit 1;
-    fi
-
-    # sort input files by bytes (C-style) and re-save them with orginal filename
-    for fileName in \
-        lexicon.txt \
-            lexicon_nosil.txt \
-            phones.txt \
-            transcripts; do
-        LC_ALL=C sort -i ${input_dir}/${fileName} -o ${input_dir}/${fileName};
-    done;
-
-    # Given dir of WAV files, create dirs for train and test, create 'wav.scp',
-    # create 'text', create 'utt2spk' and 'spk2utt', and copy the language model
-    # from elsewhere (ARPA format)
-
-    local/prepare_data.sh \
-        $audio_dir \
-        $input_dir \
-        $train_percentage \
-        $data_dir \
-        $train_dir \
-        $test_dir \
-        || printf "\n####\n#### ERROR: prepare_data.sh \n####\n\n" \
-        || exit 1;
-
-    # Copy and paste existing phonetic dictionary, language model, and phone list
-
-    local/prepare_dict.sh \
-        $data_dir \
-        $input_dir \
-        $silence_phone \
-        || printf "\n####\n#### ERROR: prepare_dict.sh\n####\n\n" \
-        || exit 1;
-
-    # Prepare a dir such as data/lang/
-    # This script can add word-position-dependent phones, and constructs a host of
-    # other derived files, that go in data/lang/.
-    # This creates the FST for the lexicon.
-
-    local/prepare_lang.sh \
-        --position-dependent-phones false \
-        ${data_dir}/local/dict \
-        ${data_dir}/local/lang \
-        ${data_dir}/lang \
-        $unknown_word \
-        || printf "\n####\n#### ERROR: prepare_lang.sh\n####\n\n" \
-        || exit 1;
-
-    # Create the FST (G.fst) for the grammar
-
-    local/prepare_lm.sh \
-        $data_dir \
-        || printf "\n####\n#### ERROR: prepare_lm.sh\n####\n\n" \
-        || exit 1;
-
-    printf "\n####===============####\n";
-    printf "#### END DATA PREP ####\n";
-    printf "####===============####\n\n";
-
 fi
 
 
@@ -192,224 +126,26 @@ if [ "$extract_feats" -eq "1" ]; then
     printf "#### BEGIN FEATURE EXTRACTION ####\n";
     printf "####==========================####\n\n";
 
-    # printf "#### MFCCs ####\n";
-
-    # for dir in $train_dir $test_dir; do
-
-    #     steps/make_mfcc.sh \
-    #         --cmd $cmd \
-    #         --nj $num_processors \
-    #         --mfcc-config ${config_dir}/mfcc.conf \
-    #         ${data_dir}/${dir} \
-    #         ${exp_dir}/make_mfcc_log/${dir} \
-    #         $mfcc_dir \
-    #         || printf "\n####\n#### ERROR: make_mfcc.sh \n####\n\n" \
-    #         || exit 1;
-
-    #     steps/compute_cmvn_stats.sh \
-    #         ${data_dir}/${dir} \
-    #         ${exp_dir}/make_mfcc_log/${dir} \
-    #         $mfcc_dir \
-    #         || printf "\n####\n#### ERROR: compute_cmvn_stats.sh \n####\n\n" \
-    #         || exit 1;
-    # done
-
-    printf "#### PLPs ####\n";
-
-    for dir in $train_dir $test_dir; do
-
-        echo "MAKE PLPs: $dir"
-        
-        steps/make_plp.sh \
-            --cmd $cmd \
-            --nj $num_processors \
-            ${data_dir}/${dir} \
-            ${exp_dir}/make_plp_log/${dir} \
-            $plp_dir
-        
-        utils/fix_data_dir.sh ${data_dir}/${dir}
-        
-        steps/compute_cmvn_stats.sh \
-            ${data_dir}/${dir} \
-            ${exp_dir}/make_plp_log/${dir} \
-            $plp_dir
-        
-        utils/fix_data_dir.sh ${data_dir}/${dir}
-    done
+    ./extract_feats.sh $data_dir/train $plp_dir 
     
-    printf "\n####========================####\n";
-    printf "#### END FEATURE EXTRACTION ####\n";
-    printf "####========================####\n\n";
-
 fi
 
 
 
-if [ "$train_monophones" -eq "1" ]; then
-
-    printf "\n####===========================####\n";
-    printf "#### BEGIN TRAINING MONOPHONES ####\n";
-    printf "####===========================####\n\n";
-
-    printf "#### Train Monophones ####\n";
-
-    steps/train_mono.sh \
-        --cmd "$cmd" \
-        --nj $num_processors \
-        --num-iters $num_iters_mono \
-        --totgauss $tot_gauss_mono \
-        --beam 6 \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/monophones \
-        || printf "\n####\n#### ERROR: train_mono.sh \n####\n\n" \
-        || exit 1;
-
-    ../../../src/gmmbin/gmm-info ${exp_dir}/monophones/final.mdl
-
-
-    printf "#### Align Monophones ####\n";
-
-    steps/align_si.sh \
-        --cmd "$cmd" \
-        --nj $num_processors \
-        --boost-silence 1.25 \
-        --beam 10 \
-        --retry-beam 40 \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/monophones \
-        ${exp_dir}/monophones_aligned \
-        || printf "\n####\n#### ERROR: align_si.sh \n####\n\n" \
-        || exit 1;
-
-    
-    printf "\n####===========================####\n";
-    printf "#### END TRAINING MONOPHONES ####\n";
-    printf "####===========================####\n\n";
-
-fi
-
-
-
-if [ "$train_triphones" -eq "1" ]; then
+if [ "$train_gmm" -eq "1" ]; then
 
     printf "\n####==========================####\n";
-    printf "#### BEGIN TRAINING TRIPHONES ####\n";
+    printf "#### BEGIN FEATURE EXTRACTION ####\n";
     printf "####==========================####\n\n";
 
-
-    printf "### Train Triphones ###\n"
-
-    steps/train_deltas.sh \
-        --cmd "$cmd" \
-        --num-iters $num_iters_tri \
-        --beam 10 \
-        $num_leaves_tri \
+    ./train_gmm.sh \
+        $data_dir \
+        $num_iters_mono \
+        $tot_gauss_mono \
+        $num_iters_tri \
         $tot_gauss_tri \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/monophones_aligned \
-        ${exp_dir}/triphones \
-        || printf "\n####\n#### ERROR: train_deltas.sh \n####\n\n" \
-        || exit 1;
-
-    ../../../src/gmmbin/gmm-info ${exp_dir}/triphones/final.mdl
-
-
-    printf "### Align Triphones ###\n"
-
-    steps/align_si.sh \
-        --cmd "$cmd" \
-        --nj $num_processors \
-        --boost-silence 1.25 \
-        --beam 10 \
-        --retry-beam 40 \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/triphones \
-        ${exp_dir}/triphones_aligned \
-        || printf "\n####\n#### ERROR: align_si.sh \n####\n\n" \
-        || exit 1;
-
-    
-    printf "\n####========================####\n";
-    printf "#### END TRAINING TRIPHONES ####\n";
-    printf "####========================####\n\n";
-
-fi
-
-
-
-if [ "$adapt_models" -eq "1" ]; then
-    
-    printf "\n####==========================####\n";
-    printf "#### BEGIN SPEAKER ADAPTATION ####\n";
-    printf "####==========================####\n\n";
-
-    printf "### Begin LDA + MLLT Triphones ###\n"
-
-    steps/train_lda_mllt.sh \
-        --cmd "$cmd" \
-        --splice-opts "--left-context=3 --right-context=3" \
         $num_leaves_tri \
-        $tot_gauss_tri \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/triphones_aligned \
-        ${exp_dir}/triphones_lda_mllt \
-        || printf "\n####\n#### ERROR: train_lda_mllt.sh \n####\n\n" \
-        || exit 1;
-
-    ../../../src/gmmbin/gmm-info ${exp_dir}/triphones_lda_mllt/final.mdl
-
-
-    printf "### Align LDA + MLLT Triphones ###\n"
-
-    steps/align_si.sh \
-        --cmd "$cmd" \
-        --nj $num_processors \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/triphones_lda_mllt \
-        ${exp_dir}/triphones_lda_mllt_aligned \
-        || printf "\n####\n#### ERROR: align_si.sh \n####\n\n" \
-        || exit 1;
-
-
-
-    printf "\n####===========================####\n";
-    printf "#### BEGIN TRAINING SAT (fMLLR) ####\n";
-    printf "####============================####\n\n";
-
-
-    printf "### Train LDA + MLLT + SAT Triphones ###\n"
-
-    steps/train_sat.sh \
-        --cmd "$cmd" \
-        $num_leaves_tri \
-        $tot_gauss_tri \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/triphones_lda_mllt_aligned \
-        ${exp_dir}/triphones_lda_mllt_sat \
-        || printf "\n####\n#### ERROR: train_sat.sh \n####\n\n" \
-        || exit 1;
-
-    ../../../src/gmmbin/gmm-info ${exp_dir}/triphones_lda_mllt_sat/final.mdl
-
-
-    printf "### Align LDA + MLLT + SAT Triphones ###\n"
-
-    steps/align_fmllr.sh \
-        --cmd "$cmd" \
-        --nj $num_processors \
-        ${data_dir}/${train_dir} \
-        ${data_dir}/lang \
-        ${exp_dir}/triphones_lda_mllt_sat \
-        ${exp_dir}/triphones_lda_mllt_sat_aligned \
-        || printf "\n####\n#### ERROR: align_si.sh \n####\n\n" \
-        || exit 1;
+        $exp_dir;
 fi
 
 
@@ -419,38 +155,9 @@ if [ "$compile_graph" -eq "1" ]; then
     printf "\n####=========================####\n";
     printf "#### BEGIN GRAPH COMPILATION ####\n";
     printf "####=========================####\n\n";
-    
-    # Graph compilation
-    
-    # This script creates a fully expanded decoding graph (HCLG) that represents
-    # the language-model, pronunciation dictionary (lexicon), context-dependency,
-    # and HMM structure in our model.  The output is a Finite State Transducer
-    # that has word-ids on the output, and pdf-ids on the input (these are indexes
-    # that resolve to Gaussian Mixture Models).
-    
-    # echo "### Compile monophone graph in ${exp_dir}/monophones/graph"
-    
-    # utils/mkgraph.sh \
-    #     --mono \
-    #     ${data_dir}/lang_test \
-    #     ${exp_dir}/monophones \
-    #     ${exp_dir}/monophones/graph \
-    #     || printf "\n####\n#### ERROR: mkgraph.sh \n####\n\n" \
-    #     || exit 1;
-        
-    echo "### Compile triphone graph in ${exp_dir}/triphones/graph"
-    
-    utils/mkgraph.sh \
-        ${data_dir}/lang_test \
-        ${exp_dir}/triphones \
-        ${exp_dir}/triphones/graph \
-        || printf "\n####\n#### ERROR: mkgraph.sh \n####\n\n" \
-        || exit 1;
-    
-    printf "\n####=======================####\n";
-    printf "#### END GRAPH COMPILATION ####\n";
-    printf "####=======================####\n\n";
 
+    compile_graph.sh
+    
 fi
 
 
@@ -460,84 +167,9 @@ if [ "$decode_test" -eq "1" ]; then
     printf "\n####================####\n";
     printf "#### BEGIN DECODING ####\n";
     printf "####================####\n\n";
-    
-    # DECODE WITH TRIPHONES WITH SAT ADJUSTED FEATURES
-    
-    # steps/decode_fmllr.sh --cmd "$cmd" \
-    #     --nj $num_processors \
-    #     ${exp_dir}/triphones_lda_mllt_sat/graph \
-    #     ${data_dir}/${test_dir} \
-    #     "${exp_dir}"'/triphones_lda_mllt_sat/decode_'"${test_dir}" \
-    #     $unknown_phone \
-    #     $silence_phone \
-    #     || exit 1;
 
-    
-    # DECODE WITH REGULAR TRIPHONES WITH VANILLA DELTA FEATURES
-
-    printf "\n ### Decoding with 4 jobs  ### "
-    
-    steps/decode.sh \
-        --cmd "$cmd" \
-        --nj 4 \
-        --beam $decode_beam \
-        --lattice-beam $decode_lattice_beam \
-        --max-active $decode_max_active_states \
-        ${exp_dir}/triphones/graph \
-        ${data_dir}/${test_dir} \
-        "${exp_dir}"'/triphones/decode_'"${test_dir}" \
-        $unknown_phone \
-        $silence_phone \
-        || printf "\n####\n#### ERROR: decode.sh \n####\n\n" \
-        || exit 1;
-    
-
-    printf "#### BEGIN CALCULATE WER ####\n";
-    
-    for x in ${exp_dir}/triphones/decode*; do
-        [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh > WER_triphones_${corpus_name}_${run}.txt;
-    done
-
-    printf "\n####==============####\n";
-    printf "#### END DECODING ####\n";
-    printf "####==============####\n\n";
-
-    echo "num_iters_mono=${num_iters_mono}" >> WER_triphones_${corpus_name}_${run}.txt
-    echo "num_iters_tri=${num_iters_tri}" >> WER_triphones_${corpus_name}_${run}.txt
-    echo "Language Model=" >> WER_triphones_${corpus_name}_${run}.txt
-    head input_${corpus_name}/task.arpabo >> WER_triphones_${corpus_name}_${run}.txt
-    
+    test_gmm.sh
 fi
-
-
-if [ "$save_model" -eq "1" ]; then
-
-    # Copy all necessary files to use new LM with this acoustic model
-    # and only necessary files to save space
-    
-    cp data_${corpus_name} ${corpus_name}_${run}
-
-    # delete unneeded files
-    rm -rf ${corpus_name}_${run}/train ${corpus_name}_${run}/test ${corpus_name}_${run}/lang_test
-
-    # copy acoustic model and decision tree to new dir
-    mkdir ${corpus_name}_${run}/model
-    cp exp_${corpus_name}/triphones/final.mdl ${corpus_name}_${run}/model/final.mdl
-    cp exp_${corpus_name}/triphones/tree ${corpus_name}_${run}/model/tree
-
-    tar -zcvf ${corpus_name}_${run}.tar.gz ${corpus_name}_${run}
-
-    # clean up
-    rm -rf ${corpus_name}_${run}
-
-    # move for storage
-    mkdir compressed_experiments
-    
-    mv ${corpus_name}_${run}.tar.gz compressed_experiments/${corpus_name}_${run}.tar.gz
-fi
-
-
-
 
 exit;
 
