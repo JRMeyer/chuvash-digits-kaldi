fb3ff1a7d956e7327ddcf2ab27f1b8ebeb353b09
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-11-15 22:45:13 +0000

    split g and l.fst prep sections

--- egs/kgz/kyrgyz-model/prep_data.sh
+++ egs/kgz/kyrgyz-model/prep_data.sh
@@ -8,7 +8,8 @@
 # 
 # INPUT:
 #
-# 
+#
+
 
 input_dir=$1
 data_dir=$2
@@ -21,27 +22,7 @@ if [ "$#" -ne 2 ]; then
 fi
 
 
-check_input=1
 prep_data=1
-
-
-if [ "$check_input" -eq "1" ]; then
-    
-    printf "\n####=================####\n";
-    printf "#### BEGIN CHECK INPUT ####\n";
-    printf "####=================####\n\n";
-
-    # sort input files by bytes (C-style) and re-save them with orginal filename
-    for fileName in \
-        lexicon.txt \
-            lexicon_nosil.txt \
-            phones.txt; do
-        LC_ALL=C sort -i $input_dir/$fileName -o $input_dir/$fileName;
-    done;
-    
-fi
-
-
 
 
 if [ "$prep_data" -eq "1" ]; then
@@ -50,9 +31,14 @@ if [ "$prep_data" -eq "1" ]; then
     printf "#### BEGIN DATA PREP ####\n";
     printf "####=================####\n\n";
 
-
-    # Copy and paste existing phonetic dictionary, and phone list
-
+    
+    echo "### Setup L.fst ###"
+    
+    for i in lexicon.txt lexicon_nosil.txt phones.txt; do
+        LC_ALL=C sort -i $input_dir/$i -o $input_dir/$i;
+    done;
+    
+    # move lexicon files
     local/prepare_dict.sh \
         $data_dir \
         $input_dir \
@@ -60,14 +46,7 @@ if [ "$prep_data" -eq "1" ]; then
         || printf "\n####\n#### ERROR: prepare_dict.sh\n####\n\n" \
         || exit 1;
 
-    # Prepare a dir such as data/lang/
-    # This script can add word-position-dependent phones, and constructs a host of
-    # other derived files, that go in data/lang/.
-    # This creates the FST for the lexicon.
-
-    # copy the language mode to the working dir (i.e. ${data_dir}/local )
-    cp $input_dir/task.arpabo $data_dir/local/lm.arpa
-
+    # create L.fst
     local/prepare_lang.sh \
         --position-dependent-phones false \
         $data_dir/local/dict \
@@ -77,13 +56,20 @@ if [ "$prep_data" -eq "1" ]; then
         || printf "\n####\n#### ERROR: prepare_lang.sh\n####\n\n" \
         || exit 1;
 
-    # Create the FST (G.fst) for the grammar
+    
 
+    echo "### Setup G.fst ###"
+
+    # move language model
+    cp $input_dir/task.arpabo $data_dir/local/lm.arpa
+
+    # create G.fst
     local/prepare_lm.sh \
         $data_dir \
         || printf "\n####\n#### ERROR: prepare_lm.sh\n####\n\n" \
         || exit 1;
 
+    
     printf "\n####===============####\n";
     printf "#### END DATA PREP ####\n";
     printf "####===============####\n\n";
