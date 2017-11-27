24e647e85d946537a3a873558a2e0003d2b5b5f4
Ubuntu <ubuntu@ip-172-31-26-96.us-west-2.compute.internal>
2017-11-27 21:40:02 +0000

    old test and chuvash

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 
+dim=500
 
 for run in {1..20}
 do
-    for dim in 100 150 200 250 300 350 400 450 500
-    do
-        ./run_nnet3_multilingual.sh "org no_voice" "tri mono" "0.5,0.5" $dim "2gram_5epoch_5layer_${dim}dim_${run}"
+    ./run_nnet3_multilingual.sh "org org" "tri tri" "0.5,0.5" $dim "2gram_10epoch_5layer_${dim}dim_${run}"
+
+    ./run_nnet3_multilingual.sh "org org" "tri mono" "0.5,0.5" $dim "2gram_10epoch_5layer_${dim}dim_${run}"
         
-    done
+    ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" $dim "2gram_10epoch_5layer_${dim}dim_${run}"
+
 done
