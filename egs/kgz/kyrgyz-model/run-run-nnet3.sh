f8bb9b0e2deb95b7cd9e010a368a19a901117cdf
Ubuntu <ubuntu@ip-172-31-45-57.us-west-2.compute.internal>
2017-12-01 15:17:34 +0000

    experiments running:)

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 
-dim=250
+dim=500
+num_epochs=10
 
-for run in {1..20}
+for run in {2..20}
 do
-    ./run_nnet3_multilingual.sh "org org" "tri tri" "0.5,0.5" $dim "2gram_2epoch_5layer_${dim}dim_${run}"
+    ./run_nnet3_multilingual.sh "org org" "tri tri" "0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}"
         
-    ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" $dim "2gram_2epoch_5layer_${dim}dim_${run}"
-    
-    ./run_nnet3_multilingual.sh "org org org org" "tri tri tri tri" "0.5,0.5,0.5,0.5" $dim "2gram_2epoch_5layer_${dim}dim_${run}"
-
-    ./run_nnet3_multilingual.sh "org org org2 org2" "tri mono tri mono" "0.5,0.5,0.5,0.5" $dim "2gram_2epoch_5layer_${dim}dim_${run}"
+    ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}"
     
 done
