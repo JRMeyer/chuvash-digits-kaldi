1ed312861c66ed2db479808ef3d68d235155edea
Ubuntu <ubuntu@ip-172-31-45-57.us-west-2.compute.internal>
2017-12-03 10:42:40 +0000

    ran test with 4 task... performance worse and mixing org - org -tri - mono failed it seems

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -4,10 +4,10 @@
 dim=500
 num_epochs=10
 
-for run in {2..20}
+for run in {1..10}
 do
-    ./run_nnet3_multilingual.sh "org org" "tri tri" "0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}"
+    ./run_nnet3_multilingual.sh "org org org org" "tri tri tri tri" "0.5,0.5,0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}"
         
-    ./run_nnet3_multilingual.sh "org org2" "tri tri" "0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}"
+    ./run_nnet3_multilingual.sh "org org2 org org2" "tri tri mono mono" "0.5,0.5,0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}"
     
 done
