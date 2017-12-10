83ad9efd6f971d4bba6d72ef67e9367dded89f9f
Ubuntu <ubuntu@ip-172-31-46-195.us-west-2.compute.internal>
2017-12-10 16:35:05 +0000

    wokring allthru prediction

--- egs/kgz/kyrgyz-model/run_forest.py
+++ egs/kgz/kyrgyz-model/run_forest.py
@@ -1,33 +1,34 @@
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import sys
+import pandas as pd
 
-train_X=sys.argv[1]
-train_Y=sys.argv[2]
-predict_X=sys.argv[3]
+train_XY=sys.argv[1]
+predict_XY=sys.argv[2]
+
+train=pd.read_csv(train_XY, sep=" ")
+train_labels = np.array( [ str(i) for i in train.iloc[:,0] ] )
+train_frames = train.iloc[:,1:].as_matrix()
 
 
-### TRAINING ###
+predict=pd.read_csv(predict_XY, sep=" ")
+predict_uttIDs = predict.iloc[:,0]
+predict_frames = predict.iloc[:,1:].as_matrix()
 
-f = open(train_X)
-X = np.loadtxt(f)
-Y = np.genfromtxt(train_Y, dtype='str' )
 
-clf = RandomForestClassifier()
+
+# ### TRAINING ###
 
 print("Training Random Forest")
-clf.fit(X,Y)
+clf = RandomForestClassifier()
+clf.fit(train_frames, train_labels)
 
 
 
-### PREDICTION ###
-
-f = open(predict_X)
-predict_data = np.loadtxt(f)
+# ### PREDICTION ###
 
 
 print("Prediction on data with Random Forest")
-
-prediction=clf.predict(predict_data)
+prediction=clf.predict(predict_frames)
 
 np.savetxt("forest_output.txt", prediction, delimiter=" ", fmt="%s")
