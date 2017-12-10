4eeca4e4505c79b152cf53325ae9710eb4d6ccde
Ubuntu <ubuntu@ip-172-31-46-195.us-west-2.compute.internal>
2017-12-10 16:35:05 +0000

    wokring allthru prediction

--- egs/kgz/kyrgyz-model/run_rand_forest.sh
+++ egs/kgz/kyrgyz-model/run_rand_forest.sh
@@ -1,26 +1,24 @@
 
-# EXTRACT ALIGNMENTS AND FEATS
+
+# FORMAT TRAIN DATA
 
 # returns <trans-id> <frame feats data>\n
-./extract_trans_ali.sh in_ali.ark in_feats.ark
+./extract_trans_ali.sh exp_org/triphones_aligned/ali.1.gz plp_org/raw_plp_train.1.ark
+
 
 # same output as above, but fewer <trans-ids>
-./get_sub_train.sh labeled_data.txt 500
+./get_rand_subset_data.sh labeled_frames.txt 500
 
-
-# TRAIN FOREST
-
-./train_python_script.py subset.trans.ali
-
-
-# RECLASSIFY
+# FORMAT PREDICT DATA
 
 # returns <utt-id> <frame feats data> in sequence
 # so that we can reconstruct ali.ark
 ./reformat_feats_ark.sh feats.ark new_txt_feats
 
-# classify those feats with new trained forest
-./classify_python_script.py new_txt_feats output.txt
+
+# TRAIN and RUN FOREST
+
+./run_forest.py sub_500_data sub_500_labels predict_data
 
 # reformat python output so it looks like original ali.ark
 ./reformat_tree_classes.sh
