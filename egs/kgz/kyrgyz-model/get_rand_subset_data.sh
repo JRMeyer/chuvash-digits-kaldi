2d4750e4633a75760ce391a7afee31224eb1db2e
Ubuntu <ubuntu@ip-172-31-46-195.us-west-2.compute.internal>
2017-12-10 16:35:05 +0000

    wokring allthru prediction

--- egs/kgz/kyrgyz-model/get_rand_subset_data.sh
+++ egs/kgz/kyrgyz-model/get_rand_subset_data.sh
@@ -22,17 +22,14 @@ cut -d " " -f 1 $all_frames_labels | \
          > sub_rand_labels
 
 # save random subset of labels and data
+
+echo ""
+echo "$0: saving random subset of data to (1) sub_rand_frames_labels"
+echo ""
+
 while read frame_id; do
     grep "^${frame_id} " $all_frames_labels >> sub_rand_frames_labels;
 done<sub_rand_labels
 rm sub_rand_labels
-
-echo ""
-echo "$0: saving random subset of data to (1) sub_${num_sub}_labels.txt"
-echo "$0:                                 (2) sub_${num_sub}_data.txt"
-echo ""
-cut -d" " -f1 sub_rand_frames_labels > sub_${num_sub}_labels.txt
-cut -d" " -f2- sub_rand_frames_labels > sub_${num_sub}_data.txt
-rm sub_rand_frames_labels
 
 echo "$0: DONE"
