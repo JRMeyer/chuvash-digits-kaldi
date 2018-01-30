5f1b94d9b79fb118eb2fa4dd2bae5ade6af4568a
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-01-30 17:27:44 +0000

    bug in run_nnet3_multilingual and all gmms for libri worked!

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -6,8 +6,6 @@ num_epochs=1
 
 for run in {1..1}
 do
-     ./run_nnet3_multilingual.sh "new new" "tri tri" "0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}" "891 891"
-    
-#    ./run_nnet3_multilingual.sh "org org" "tri tri" "0.5,0.5" $dim $num_epochs "1gram_${num_epochs}epoch_5layer_${dim}dim_${run}" "tree tree"
-        
+     ./run_nnet3_multilingual.sh "libri-org libri-org" "tri tri" "0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${run}" "tree tree"
+            
 done
