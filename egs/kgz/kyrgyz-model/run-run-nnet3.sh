2de63d4c5507a96610f0001a145f4802e4c44d71
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-02-02 16:22:07 +0000

    cannot do bootstrap with replacement... nnet3 trainging throws fit if it finds duplicates

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -4,8 +4,14 @@
 dim=100
 num_epochs=5
 
+./run_nnet3_multilingual.sh "libri-org libri-boot" "tri tri" "0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_1" "tree tree"
+
+exit
+
+
 for run in {1..1}
 do
+     
      ./run_nnet3_multilingual.sh "libri-org libri-org libri-org" "tri tri tri" "0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${run}" "tree tree tree"
      ./run_nnet3_multilingual.sh "libri-org libri-org libri-org libri-org" "tri tri tri tri" "0.5,0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${run}" "tree tree tree tree"
 
