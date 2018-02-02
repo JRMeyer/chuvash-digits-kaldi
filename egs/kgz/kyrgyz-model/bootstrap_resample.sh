96c349dced9ff1b8d8f08807456fde6115865dfe
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-02-02 16:22:07 +0000

    cannot do bootstrap with replacement... nnet3 trainging throws fit if it finds duplicates

--- egs/kgz/kyrgyz-model/bootstrap_resample.sh
+++ egs/kgz/kyrgyz-model/bootstrap_resample.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+# Joshua Meyer 2018
+
+# input: egs.scp file output by prepare_multilingual_egs.sh (just get_egs.sh wrapper)
+# output: scp file of same length, but resampled with replacement
+#
+#
+#
 
 infile=$1
 outfile=$2
 
 num_egs=(`wc -l $infile`)
 
+proc_ids=()
 iter=0
 while [ $iter -lt $num_egs ]; do
-    ((iter++))
-    shuf $infile -n 1 >> $outfile
+    ((iter++));
+    shuf $infile -n 1 >> $outfile &
+    proc_ids+=($!)
 done
+for proc_id in ${proc_ids[*]}; do wait $proc_id; done;
+
 
 LC_ALL=C sort $outfile -o $outfile
+
