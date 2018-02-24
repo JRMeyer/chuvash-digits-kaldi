6bae1b885aeb3d3fec5cefdf1f4f4f56ac7ee330
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-02-24 08:33:12 +0000

    bootstrap experiments... no real improvement with 25percent even at 3 extra tasks

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -3,34 +3,35 @@
 
 gmm=$1
 
-dim=100
-num_epochs=1
+dim=200
+num_epochs=5
 
 
 
 
     
 
+################
+### BASELINE ###
+################
 
-for dnn in {1..2}
+
+### A + B ###
+## 2 baselines 
+./run_nnet3_multilingual.sh "libri-org libri-org" "tri tri" "0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree" "0"
+
+### A + B + C ###
+## 3 baselines 
+./run_nnet3_multilingual.sh "libri-org libri-org libri-org" "tri tri tri" "0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree" "0"
+
+### A + B + C + D ###
+## 4 baselines
+./run_nnet3_multilingual.sh "libri-org libri-org libri-org libri-org" "tri tri tri tri" "0.5,0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree tree" "0"
+
+
+for dnn in {1..5}
 do
 
-    ################
-    ### BASELINE ###
-    ################
-
-    
-    ### A + B ###
-    ## 2 baselines 
-    ./run_nnet3_multilingual.sh "libri-org libri-org" "tri tri" "0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree" "0"
-
-    ### A + B + C ###
-    ## 3 baselines 
-    # ./run_nnet3_multilingual.sh "libri-org libri-org libri-org" "tri tri tri" "0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree" "0"
-
-    ### A + B + C + D ###
-    ## 4 baselines
-    # ./run_nnet3_multilingual.sh "libri-org libri-org libri-org libri-org" "tri tri tri tri" "0.5,0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree tree" "0"
 
     ############
     ### LING ###
@@ -93,22 +94,14 @@ do
     ### A + B ###
     ## 25per
     ./run_nnet3_multilingual.sh "libri-org libri-boot1Quarter" "tri tri" "0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree" ".25"
-    ## 50per
-    ./run_nnet3_multilingual.sh "libri-org libri-boot2Quarter" "tri tri" "0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree" ".50"  
-    ## 75per
-    ./run_nnet3_multilingual.sh "libri-org libri-boot3Quarter" "tri tri" "0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree" ".75"  
 
     # ### A + B + C ###
     # ## 25per + 50per
-    # ./run_nnet3_multilingual.sh "libri-org libri-boot1Quarter libri-boot2Quarter" "tri tri tri" "0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree" "1"
-    # ## 50per + 75per
-    # ./run_nnet3_multilingual.sh "libri-org libri-boot2Quarter libri-boot3Quarter" "tri tri tri" "0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree" "1"  
-    # ## 75per + 25per
-    # ./run_nnet3_multilingual.sh "libri-org libri-boot3Quarter libri-boot1Quarter" "tri tri tri" "0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree" "1"  
+    ./run_nnet3_multilingual.sh "libri-org libri-boot1Quarter libri-boot2Quarter" "tri tri tri" "0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree" ".25,.25"
 
     # ### A + B + C + D ###
     # ## 25per + 50per + 75per
-    # ./run_nnet3_multilingual.sh "libri-org libri-boot1Quarter libri-boot2Quarter libri-boot3Quarter" "tri tri tri tri" "0.5,0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree tree" "1"
+    ./run_nnet3_multilingual.sh "libri-org libri-boot1Quarter libri-boot2Quarter libri-boot3Quarter" "tri tri tri tri" "0.5,0.5,0.5,0.5" $dim $num_epochs "2gram_${num_epochs}epoch_5layer_${dim}dim_${gmm}_${dnn}" "tree tree tree tree" ".25,.25,.25"
     
     
 done
