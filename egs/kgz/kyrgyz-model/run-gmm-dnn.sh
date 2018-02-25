777c5e18ec167b4dfa5d834e4bd5e8757febe8fd
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-02-25 13:24:56 +0000

    going for broke

--- egs/kgz/kyrgyz-model/run-gmm-dnn.sh
+++ egs/kgz/kyrgyz-model/run-gmm-dnn.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
 
-for gmm in {1..2}; do
 
 
+for gmm in {1..100}; do
 
-    echo "#####################"
-    echo "### TRAIN NEW GMM ###"
-    echo "#####################"
-
-    
-    
-    # clean up from last gmm run
-    rm -rf data_* exp_* plp_*
-
-    for corpus in libri-org libri-boot1Quarter libri-boot2Quarter libri-boot3Quarter; do
-        
-        # clean up from last gmm run
-        rm -rf input_$corpus/audio input_$corpus/phones.txt input_$corpus/transcripts
-
-        # use env -i to run script in clean environment
-        env -i ./run_gmm.sh $corpus $gmm
-
-    done
-    
-
-    
-    echo "#####################"
-    echo "### TRAIN NEW DNN ###"
-    echo "#####################"
-
-    # clean up from last dnn run
-    rm -rf exp data plp
-    
-    ./setup_multitask.sh "libri-org libri-boot1Quarter libri-boot2Quarter libri-boot3Quarter"
-
-    env -i ./run-run-nnet3.sh $gmm
+    env -i ./run_gmm.sh libri-boot$num 1
     
 done
+
+env -i ./run_gmm.sh libri-org 1
+
+sudo shutdown now
+
+# for gmm in {1..100}; do
+
+
+
+#     echo "#####################"
+#     echo "### TRAIN NEW GMM ###"
+#     echo "#####################"
+
+    
+    
+#     # clean up from last gmm run
+#     rm -rf data_* exp_* plp_*
+
+#     for corpus in libri-org libri-boot1Quarter libri-boot2Quarter libri-boot3Quarter; do
+        
+#         # clean up from last gmm run
+#         rm -rf input_$corpus/audio input_$corpus/phones.txt input_$corpus/transcripts
+
+#         # use env -i to run script in clean environment
+#         env -i ./run_gmm.sh $corpus $gmm
+
+#     done
+    
+
+    
+#     echo "#####################"
+#     echo "### TRAIN NEW DNN ###"
+#     echo "#####################"
+
+#     # clean up from last dnn run
+#     rm -rf exp data plp
+    
+#     ./setup_multitask.sh "libri-org libri-boot1Quarter libri-boot2Quarter libri-boot3Quarter"
+
+#     env -i ./run-run-nnet3.sh $gmm
+    
+# done
 
 
