ecb730c11dc48aeffae01544bc046759293a5b67
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-02-26 09:00:33 +0000

    moving generated files to /data/MTL/

--- egs/kgz/kyrgyz-model/setup_multitask.sh
+++ egs/kgz/kyrgyz-model/setup_multitask.sh
@@ -28,25 +28,51 @@
 
 
 
-langs=$1
 
+
+
+to_dir=$1
+from_dir=$2
+langs=$3
 langs=( $langs )
 
 for lang in ${langs[@]}; do
-    mkdir -p data/$lang exp/$lang;
+    mkdir -p $to_dir/data/$lang $to_dir/exp/$lang;
     
-    cd data/$lang;
-    ln -s ../../data_${lang}/train/ train
-    ln -s ../../data_${lang}/lang/ lang
+    cd $to_dir/data/$lang;
+    ln -s $from_dir/data_${lang}/train/ train
+    ln -s $from_dir/data_${lang}/lang/ lang
     
-    cd ../../exp/$lang;
-    ln -s ../../exp_${lang}/triphones tri
-    ln -s ../../exp_${lang}/triphones_aligned tri_ali
-    ln -s ../../exp_${lang}/monophones mono
-    ln -s ../../exp_${lang}/monophones_aligned mono_ali
+    cd $to_dir/exp/$lang;
+    ln -s $from_dir/exp_${lang}/triphones tri
+    ln -s $from_dir/exp_${lang}/triphones_aligned tri_ali
+    ln -s $from_dir/exp_${lang}/monophones mono
+    ln -s $from_dir/exp_${lang}/monophones_aligned mono_ali
 
-    cd ../../
 done
+
+
+
+
+# langs=$1
+
+# langs=( $langs )
+
+# for lang in ${langs[@]}; do
+#     mkdir -p data/$lang exp/$lang;
+    
+#     cd data/$lang;
+#     ln -s ../../data_${lang}/train/ train
+#     ln -s ../../data_${lang}/lang/ lang
+    
+#     cd ../../exp/$lang;
+#     ln -s ../../exp_${lang}/triphones tri
+#     ln -s ../../exp_${lang}/triphones_aligned tri_ali
+#     ln -s ../../exp_${lang}/monophones mono
+#     ln -s ../../exp_${lang}/monophones_aligned mono_ali
+
+#     cd ../../
+# done
 
 
     
