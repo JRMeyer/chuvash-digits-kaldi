a7d83f184b75ff7a6c17178b02d59e4ae7b7141b
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-04-01 15:51:42 +0000

    manner

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -20,9 +20,8 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
+./run_nnet3_multilingual.sh "libri-org libri-place" "tri tri" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_PLACE" "tree tree" "0"
 
-# baseline
-./run_nnet3_multilingual.sh "libri-org" "tri" "1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_BASELINE" "tree" "0"
 
 rm -rf /data/MTL/exp/nnet3
 rm -rf /data/MTL/exp/libri-org/nnet3
@@ -30,9 +29,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-
-
-./run_nnet3_multilingual.sh "libri-org libri-voice" "tri tri" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_VOICE" "tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-manner" "tri tri" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_MANNER" "tree tree" "0"
 
 
 exit
