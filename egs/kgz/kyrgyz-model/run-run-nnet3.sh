73cca15790ac3472ec4c04f2ed0a7ac2633aea83
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-04-03 16:18:29 +0000

    voice place

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -13,7 +13,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "atai-org libri-org" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "atai-org libri-voice libri-place" "tri mono mono" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -23,7 +23,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "atai-org libri-voice" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "atai-org libri-voice libri-manner" "tri mono mono" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -33,7 +33,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "atai-org libri-manner" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "atai-org libri-place libri-manner" "tri mono mono" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -43,7 +43,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "atai-org libri-place" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "atai-org libri-place libri-manner libri-voice" "tri mono mono mono" "1.0,1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree tree" "0"
 
 
 
