3bf0a532bee8b43e7a94d1101b1f276f4bdd563e
Josh Meyer <joshuameyer@email.arizona.edu>
2018-04-03 04:06:52 -0500

    cleaning up

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -1,4 +1,4 @@
-\#!/bin/bash
+#!/bin/bash
 
 
 dim=500
@@ -12,10 +12,14 @@ num_epochs=2
 
 
 
+rm -rf /data/MTL/exp/nnet3
+rm -rf /data/MTL/exp/atai-org/nnet3
+rm -rf /data/MTL/exp/libri-org/nnet3
+rm -rf /data/MTL/exp/libri-place/nnet3
+rm -rf /data/MTL/exp/libri-manner/nnet3
+rm -rf /data/MTL/exp/libri-voice/nnet3
 
-
-
-
+./run_nnet3_multilingual.sh "atai-org libri-org" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -25,19 +29,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "atai-org libri-place libri-manner" "tri tri tri" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
-
-
-
-rm -rf /data/MTL/exp/nnet3
-rm -rf /data/MTL/exp/atai-org/nnet3
-rm -rf /data/MTL/exp/libri-org/nnet3
-rm -rf /data/MTL/exp/libri-place/nnet3
-rm -rf /data/MTL/exp/libri-manner/nnet3
-rm -rf /data/MTL/exp/libri-voice/nnet3
-
-./run_nnet3_multilingual.sh "atai-org libri-manner libri-voice" "tri tri tri" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
-
+./run_nnet3_multilingual.sh "atai-org libri-voice" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -47,7 +39,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "atai-org libri-voice libri-place" "tri tri tri" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
+./run_nnet3_multilingual.sh "atai-org libri-manner" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -57,7 +49,9 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "atai-org libri-voice libri-place libri-manner" "tri tri tri tri" "1.0,1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree tree" "0"
+./run_nnet3_multilingual.sh "atai-org libri-place" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+
+
 
 
 exit
