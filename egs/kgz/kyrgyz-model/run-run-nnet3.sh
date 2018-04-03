21d00a4a1136c047634a0bc80718805755faf391
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-04-03 10:00:44 +0000

    mono libri-org

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -4,12 +4,6 @@
 dim=500
 num_epochs=2
 
-###
-### librispeech has 4.86 hours and atai has 1.59
-###  1:2 == 1.53
-###  1:1 == 3.06
-###  2:1 == 6.12
-
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -55,28 +49,3 @@ rm -rf /data/MTL/exp/libri-voice/nnet3
 
 
 exit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# tripleWeight=( `echo "$weight * 3" | bc` )
-# # mono half tri
-# ./run_nnet3_multilingual.sh "atai libri-mono libri-half libri-tri" "tri mono tri tri" "${tripleWeight},1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_weight-${dnn}" "tree tree tree tree" "0"
-
-
