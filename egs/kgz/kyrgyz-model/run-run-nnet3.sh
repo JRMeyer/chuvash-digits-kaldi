964d20ddfc059dca1b3102766948ee5f72b0e74d
Ubuntu <ubuntu@ip-172-31-41-122.us-west-2.compute.internal>
2018-04-04 12:46:56 +0000

    cleaning

--- egs/kgz/kyrgyz-model/run-run-nnet3.sh
+++ egs/kgz/kyrgyz-model/run-run-nnet3.sh
@@ -21,7 +21,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "libri-org libri-place" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-place" "tri mono" "1.0,0.33" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_oneThirdWeight" "tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -31,7 +31,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "libri-org libri-voice" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-voice" "tri mono" "1.0,0.33" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_oneThirdWeight" "tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -41,7 +41,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "libri-org libri-manner" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-manner" "tri mono" "1.0,0.33" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_oneThirdWeight" "tree tree" "0"
 
 
 
@@ -56,7 +56,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "libri-org libri-voice libri-place" "tri mono mono" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-voice libri-place" "tri mono mono" "1.0,0.33,0.33" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_oneThirdWeight" "tree tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -66,7 +66,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "libri-org libri-voice libri-manner" "tri mono mono" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-voice libri-manner" "tri mono mono" "1.0,0.33,0.33" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_oneThirdWeight" "tree tree tree" "0"
 
 
 rm -rf /data/MTL/exp/nnet3
@@ -76,7 +76,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "libri-org libri-place libri-manner" "tri mono mono" "1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-place libri-manner" "tri mono mono" "1.0,0.33,0.33" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_oneThirdWeight" "tree tree tree" "0"
 
 
 
@@ -89,26 +89,7 @@ rm -rf /data/MTL/exp/libri-place/nnet3
 rm -rf /data/MTL/exp/libri-manner/nnet3
 rm -rf /data/MTL/exp/libri-voice/nnet3
 
-./run_nnet3_multilingual.sh "libri-org libri-place libri-manner libri-voice" "tri mono mono mono" "1.0,1.0,1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree tree tree" "0"
-
-
-
-
-
-
-
-
-
-
-
-rm -rf /data/MTL/exp/nnet3
-rm -rf /data/MTL/exp/atai-org/nnet3
-rm -rf /data/MTL/exp/libri-org/nnet3
-rm -rf /data/MTL/exp/libri-place/nnet3
-rm -rf /data/MTL/exp/libri-manner/nnet3
-rm -rf /data/MTL/exp/libri-voice/nnet3
-
-./run_nnet3_multilingual.sh "libri-org libri-org" "tri mono" "1.0,1.0" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim" "tree tree" "0"
+./run_nnet3_multilingual.sh "libri-org libri-place libri-manner libri-voice" "tri mono mono mono" "1.0,0.33,0.33,0.33" $dim $num_epochs "2gramWiki_${num_epochs}epoch_5layer_${dim}dim_oneThirdWeight" "tree tree tree tree" "0"
 
 
 
